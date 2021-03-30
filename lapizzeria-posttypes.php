9b1c33680a60c5cbcf1200745b64c854d1213436
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-03-30 18:00:07 -0500

    [+] Plugin: Metabox 'precio' para Especialidades

--- lapizzeria-posttypes.php
+++ lapizzeria-posttypes.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       La Pizzeria: Especialidades
  * Plugin URI:        
- * Description:       Agrega Especialidades al sitio web La Pizzería (Custom Post Types)
+ * Description:       Agrega Especialidades al sitio web La Pizzería (Custom Post Types, Meta Box)
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
@@ -13,6 +13,8 @@
 */
 
 if( ! defined( 'ABSPATH' ) )  exit;     //  Evita acceso al codigo del plugin
+
+add_action( 'init', 'lapizzeria_add_cpt_specialties' );
 
 /** Define Custom Post Type para Especialidades de La Pizzeria */
 function lapizzeria_add_cpt_specialties() {
@@ -53,5 +55,84 @@ function lapizzeria_add_cpt_specialties() {
     #   Registra el Custom Post Type
 	register_post_type( 'specialties', $args );
 }
-add_action( 'init', 'lapizzeria_add_cpt_specialties' );
+
+add_action( 'add_meta_boxes', 'lapizzeria_add_meta_boxes' );
+
+/** Crear un MetaBox */
+function lapizzeria_add_meta_boxes() {
+
+    add_meta_box( 
+        'lapizzeria-price',                                 #   ID unico de identificacion
+        _x( 'Additional Information', 'lapizzeria' ),       #   Titulo para el Metabox
+        'lapizzeria_meta_box_html_price',                   #   Callback: Funcion que dibujará formulario para el Metabox
+        array( 'specialties' ),                             #   Nombre del Post o los Post a los que se agregará el Metabox
+        'normal',                                           #   Contexto dentro de la pantalla donde debe mostrarse el cuadro: 'normal', 'side', and 'advanced'. Valor por defecto: 'advanced'
+        'high',                                             #   La prioridad dentro del contexto donde debe mostrarse el cuadro: 'high', 'core', 'default', or 'low'. Valor por defecto: 'default'
+        null                                                #   Datos que deben establecerse como la propiedad $ args de la matriz de caja (que es el segundo parámetro que se pasa a su devolución de llamada). Valor por defecto: null
+    );
+}
+
+/** Despliega los campos que tendra el metabox en su formulario */
+function lapizzeria_meta_box_html_price( $current_post ) {
+    
+    #   Agrega un nonce a un formulario
+    wp_nonce_field( 
+        basename( __FILE__ ),       #   Nombre del archivo actual
+        'mb_nonce_specialities'     #   Nombre temporal para el formulario
+    );
+
+    $value = get_post_meta( 
+        $current_post -> ID,    #   ID del Post
+        'price',                #   Nombre del campo 
+        true                    #   Si se debe devolver un solo valor. Este parámetro no tiene ningún efecto si no se especifica $key. Valor predeterminado: falso
+    );
+    $value = $value != '' ? $value : 0;
+
+    ?>  
+        <p><?php _e( 'Register the sale price of this specialty', 'lapizzeria' ); ?></p>
+        <label for="price"><?php _e( 'Price', 'lapizzeria' ); ?></label>
+        <input 
+            id="price" 
+            type="number" 
+            name="price" 
+            placeholder="<?php _e( 'Price for this specialty', 'lapizzeria' ); ?>" 
+            min="0"
+            value="<?php echo $value; ?>"
+        />
+    <?php
+}
+
+/** Guardar datos de un MetaBox */
+function lapizzeria_save_meta_boxes( $post_id, $current_post, $update ) {
+
+    // #   Verifica si NO puede validar el nonce del formulario
+    if( ! isset( $_POST[ 'mb_nonce_specialities' ] ) || ! wp_verify_nonce( $_POST[ 'mb_nonce_specialities' ], basename( __FILE__ ) ) ) {
+        return $post_id;
+    }
+
+    #   Verifica si NO puede el usuario editar el post
+    if( ! current_user_can( 'edit_post', $post_id ) ) {
+        return $post_id;
+    }
+
+    #   Verifica si esta definido el DOING_AUTOSAVE
+    if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+        return $post_id;
+    }
+
+    #   Define variable con un valor por defecto
+    $price = 0;
+
+    #   Verifica que el valor de la variable obtenida del formulario esta definida
+    if( isset( $_POST[ 'price' ] ) ) {
+        $price = abs( $_POST[ 'price' ] );
+    }
+    update_post_meta( 
+        $post_id,   #   ID del Post
+        'price',    #   Nombre del campo
+        $price      #   Valor a actualizar
+    );
+    
+}
+add_action( 'save_post', 'lapizzeria_save_meta_boxes', 10, 3 );
 
